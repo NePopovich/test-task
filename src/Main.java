7fe6a23c61d3034796d6803687f34dfdc7758763
Aleksey Larkov <djalekseeff@gmail.com>
2025-02-22 23:31:32 +0300

    Рабочая версия строчного калькулятора на одну операцию

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,65 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static String calc(String input) throws Exception {
+        input = input.replaceAll(" ", "");
+        if (checkString(input)){
+            Operators operator = getOperator(input);
+            String op = "\\" + operator.toString();
+            Integer firstNumb = Integer.valueOf(input.split(op)[0]);
+            Integer secondNumb = Integer.valueOf(input.split(op)[1]);
+            switch (operator){
+                case PLUS:
+                    return String.valueOf(firstNumb + secondNumb);
+                case MINUS:
+                    return String.valueOf(firstNumb - secondNumb);
+                case MULTIPLY:
+                    return String.valueOf(firstNumb * secondNumb);
+                case DIVISION:
+                    return String.valueOf(firstNumb / secondNumb);
+            }
+        }else {
+            throw new Exception("Неверное верыжение!");
+        }
+        return null;
+    }
+
+    private static boolean checkString(String string) {
+        for (Operators op : Operators.values()) {
+            if (string.contains(op.toString())) {
+                String operator = "\\" + op.toString();
+                String[] onlyNumbs = string.split(operator);
+                if (onlyNumbs.length == 2) {
+                    if (Integer.valueOf(onlyNumbs[0]) >= 1 &&
+                            Integer.valueOf(onlyNumbs[1]) >= 1 &&
+                            Integer.valueOf(onlyNumbs[0]) <= 10 &&
+                            Integer.valueOf(onlyNumbs[1]) <= 10) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private static Operators getOperator(String string){
+        for (Operators op : Operators.values()){
+            if (string.contains(op.toString())){
+                return op;
+            }
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+            Scanner in = new Scanner(System.in);
+            System.out.print("Введите выражение: ");
+            String procedure = in.nextLine();
+            try {
+                System.out.println("Результат = " + calc(procedure));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
     }
 }
---
+++ src/Operators.java
@@ -0,0 +1,18 @@
+public enum Operators {
+    PLUS("+"), MINUS("-"), MULTIPLY("*"), DIVISION("/");
+
+    private final String title;
+
+    Operators(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+}
